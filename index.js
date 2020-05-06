eaa171140ce46271dba509b46e4ec6f46b3cf91e
Onilton Maciel <oniltonmaciel@gmail.com>
2020-05-06 12:36:24 -0300

    Add wireframe option

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const OPTIONS = ['state', 'darkMode', 'removeZeroes', 'barMetric', 'colorMetric', 'days', 'elevationMultiplier', 'opacity'];
+const OPTIONS = ['state', 'darkMode', 'removeZeroes', 'barMetric', 'colorMetric', 'days', 'elevationMultiplier', 'opacity', 'wireframe'];
 
 var stateIdByUF = { "RO": 11, "AC": 12, "AM": 13, "RR": 14, "PA": 15, "AP": 16, "TO": 17, "MA": 21, "PI": 22, "CE": 23, "RN": 24, "PB": 25, "PE": 26, "AL": 27, "SE": 28, "BA": 29, "MG": 31, "ES": 32, "RJ": 33, "SP": 35, "PR": 41, "SC": 42, "RS": 43, "MS": 50, "MT": 51, "GO": 52, "DF": 53 }
 
@@ -460,7 +460,7 @@ async function doIt() {
             stroked: false,
             filled: true,
             extruded: true,
-            wireframe: true,
+            wireframe: options.wireframe,
             fp64: true,
             // lineWidthUnits: 'pixels',
             // lineWidthMaxPixels: 0,
